79a4a1d466a8fa49f2b00c0b58d09434264d3f95
Akhil Surya <60837182+AkhilSurya-2124@users.noreply.github.com>
2020-03-26 12:44:59 +0530

    Update OS1.c

--- OS1.c
+++ OS1.c
@@ -5,7 +5,7 @@ int main()
 	int n,q;// n is number of processes
 	//	   q is time quantum
 	int over =0;//used to specify if the given process is completed or not
-	printf("Enter number of processes: ");
+	printf("Enter number of processes:\t\t");
 	scanf("%d",&n);
 	int a[n];// array containing arrival time 
 	int b[n];//array containing burst time
@@ -15,19 +15,21 @@ int main()
 	int wait=0,turnaround=0;//wait is waiting time and turnaround is turnaround time
 	int burst[n];//to store burst time , used for calculations later
 	for ( i=0;i<n;i++)
-	{
-		printf("Enter arrival time of process %d :\t",i+1);
+	{	
+		
+		printf("\nEnter arrival time of process %d :\t",i+1);
 		scanf("%d",&a[i]);
-		printf("Enter burst time of process %d :\t",i+1);
+		printf("\n Enter  burst time of process %d :\t",i+1);
 		scanf("%d",&b[i]);
+	
 		burst[i]=b[i];//copying because we need to do furthur calculations
 	}
-	printf("Enter time quantum:\t");
+	printf("\nEnter time quantum:\t");
 	scanf("%d",&q);
 	int rem;//remaining processes
 	i=0;
 	rem=n;//to retain the value of umber of processes
-	printf("Order of executing :\n");
+	printf("\nOrder of execution :\n");
 	while(rem>0)//loop until all the processes are executed
 	{over=0;
 	if (a[i]>et){// only do processes which have been arrived at the current time
@@ -41,7 +43,7 @@ int main()
 			et=q+et;
 			b[i]=b[i]-q;
 			//printf("CURRENT TIME  %d\n",et);
-			printf("P[%d]\t",i+1);
+			printf("P[%d]\t\t",i+1);
 		}
 		else if (a[i]<=et && b[i]>0 && (b[i]-q)<=0)
 		{
@@ -49,7 +51,7 @@ int main()
 			et=et+b[i];
 			b[i]=0;
 			over =1;
-			printf("P[%d]\t",i+1);
+			printf("P[%d]\t\t",i+1);
 			//printf("current time: %d\n process %d is over",et,i);
 		}
 		if (b[i]==0 && over==1)//to check if the process is completed
@@ -71,7 +73,7 @@ int main()
 		}
 		}
 }
-printf("\nProcess\t\tBurst Time\t\tWaiting Time\t\tTurnaround Time\n");
+printf("\n\n\nProcess\t\tBurst Time\t\tWaiting Time\t\tTurnaround Time\n");
 
 for (i=0;i<n;i++)
 {
@@ -82,6 +84,6 @@ for (i=0;i<n;i++)
     float average_turnaround_time ;
 	average_turnaround_time= turnaround*1.0/n;
       printf("Average Waiting Time: %f\n", average_wait_time);
-      printf("/nAvg Turnaround Time: %f", average_turnaround_time);
+      printf("\nAvg Turnaround Time: %f", average_turnaround_time);
       return 0;
 }
